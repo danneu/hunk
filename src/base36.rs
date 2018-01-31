446ba293afce4fd7346a35ab8b1a790c170af199
Dan Neumann <danrodneu@gmail.com>
2018-01-31 12:31:23 -0600

    Improve base36::encode
    - Removed the allocation
    - Encodes in correct order

--- src/base36.rs
+++ src/base36.rs
@@ -1,16 +1,28 @@
-// Encodes backwards, but no point in reversing it since it's just for ETags.
+// Used for ETags
+
 pub fn encode(mut n: u64) -> String {
-    let cap = f64::from(36).log(n as f64).ceil() as usize; // max 13
-    let mut buf = Vec::with_capacity(cap);
-    while n >= 36 {
-        buf.push(ALPHABET[(n % 36) as usize]);
-        n /= 36
+    let cap = 1 + if n == 0 {
+        // special case since log(36, 0) is -∞
+        0
+    } else {
+        (n as f64).log(BASE as f64).floor() as usize
+    };
+    let buf = &mut ['0'; MAX_CAP];
+    let mut i = MAX_CAP - 1;
+    while n >= BASE {
+        buf[i] = ALPHABET[(n % BASE) as usize];
+        i -= 1;
+        n /= BASE
     }
-    buf.push(ALPHABET[n as usize]);
-    buf.into_iter().collect()
+    buf[i] = ALPHABET[n as usize];
+    buf[(MAX_CAP - cap)..].into_iter().collect()
 }
 
 // PRIVATE
+
+const BASE: u64 = 36;
+
+const MAX_CAP: usize = 13;
 
 const ALPHABET: &[char] = &[
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
